52f5339d0e86e85475aba02d9674ea0729387ae7
cerisesav <victoriabaksarchi@gmail.com>
2024-09-13 00:22:49 +0300

    One buffer
    Fread, fseek, one buffer, replacement \n to \0

--- main.cpp
+++ main.cpp
@@ -5,49 +5,91 @@
 
 int compare_string(const char* first_string, const char* next_string);
 void sorter(char* mass[], int line);
+int size_text(FILE *text);
 
 int main()
 {
-    const int line = 15;
-    const int len = 100;
+    FILE *text = fopen("text.txt", "rb");
+    const int text_bytes = size_text(text);
 
-    char text_a[line][len] = {};
-    char *ptr[line] = {};
+    int *ptr = NULL;
 
-    FILE *text = fopen("text.txt", "r");
+    char* text_a = (char*)calloc(text_bytes + 1, sizeof(char));
+    const size_t count = fread(text_a, sizeof(text_a[0]), text_bytes, text);
 
-    int i = 0;
-
-    while (i < line && fgets(text_a[i], len, text))
+    int line_count = 0;
+    for (int i = 0; i < count; i++)
     {
-        ptr[i] = text_a[i];
-        i++;
+        if (text_a[i] == '\n')
+        {
+            line_count++;
+        }
+    }
+
+    char** lines = (char**)calloc(line_count + 1, sizeof(char*));
+
+    lines[0] = text_a;
+
+    int j = 1;
+    for (int i = 0; i < count; i++)
+    {
+        if (text_a[i] == '\n')
+        {
+            text_a[i] = '\0';
+            if (i + 1 < count)
+            {
+                lines[j++] = &text_a[i + 1];
+            }
+        }
+    }
+
+    sorter(lines, line_count);
+
+    for (int i = 0; i < line_count; i++) {
+        printf("%s\n", lines[i]);
     }
 
     fclose(text);
-
-    // int result = compare_string(text_a[13], text_a[14]);
-    // printf("%d", result);
-
-    sorter(ptr, i);
-
-     for (int j = 0; j < i; j++)
-    {
-        printf("%s\n", ptr[j]);
-    }
+    free(text_a);
+    free(ptr);
 
     return 0;
 }
 
+int size_text(FILE *text)
+{
+    fseek(text, 0, SEEK_END);
+    int lSize = ftell(text);
+    rewind(text);
+    return lSize;
+}
 
 int compare_string(const char* first_string, const char* next_string)
 {
-    int i = 0;
+    int i = 0, j = 0;
 
-    while ((first_string[i] != '\0') && (tolower(first_string[i]) == tolower(next_string[i])))
+    while (first_string[i] != '\0' && next_string[j] != '\0')
+    {
+
+        while (first_string[i] != '\0' && !isalnum(first_string[i]))
+        {
+            i++;
+        }
+        while (next_string[j] != '\0' && !isalnum(next_string[j]))
+        {
+            j++;
+        }
+
+        if (tolower(first_string[i]) != tolower(next_string[j]))
+        {
+            return ((int)tolower(first_string[i]) - (int)tolower(next_string[j]));
+        }
+
         i++;
+        j++;
+    }
 
-    return ((int)first_string[i] - (int)next_string[i]);
+    return ((int)first_string[i] - (int)next_string[j]);
 }
 
 void sorter(char* mass[], int line)
