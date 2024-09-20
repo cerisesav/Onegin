33dc8c1a512fa1dc73e62a3ab105d4a293095421
cerisesav <victoriabaksarchi@gmail.com>
2024-09-20 16:47:21 +0300

    qsort, structures

--- sort_compare.h
+++ sort_compare.h
@@ -4,7 +4,7 @@
 int compare_string(const char* first_string, const char* next_string);
 
 void sort_strings(char** mass, int line);
-void quickSort(char** array, int low, int high);
+void quick_sort(char** array, int low, int high);
 void swap(void *a, void *b, size_t width);
 int back_compare_string(const char* first_string, const char* next_string);
 void skip_chars_straight(size_t i, size_t j, const char* first_char, const char* second_char);
